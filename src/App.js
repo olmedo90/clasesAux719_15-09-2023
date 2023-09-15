35d3ccb6a48a109efe0f5be48417d3b3506e967b
olmedo90 <olmedito.90@gmail.com>
2023-09-15 15:28:34 -0400

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>no entendido Git meresco una practica</h1>
+    </div>
+  );
+}
+
+export default App;
